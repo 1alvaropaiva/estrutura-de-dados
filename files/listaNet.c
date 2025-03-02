da7355602b5cc7822c338cdaed801a2e296f1f31
1alvaropaiva <alvaropaiva.dev@gmail.com>
2025-03-02 00:58:23 -0300

    att: readme

--- files/listaNet.c
+++ files/listaNet.c
@@ -82,4 +82,101 @@ int main () {
 
     return 0;
 }
+-----------------------------------------------------------------------------------------------------------------------
+04)
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char matricula[10];
+    char nome[100];
+    float nota1, nota2, nota3;
+}Alunos;
+
+int main() {
+    Alunos aluno[3];
+    float maiorNota = 0, maiorMedia = 0, menorMedia = 20;
+    int maiorMediaAluno=0, menorMediaAluno=0, maiorNotaAluno=0;
+    for (int i=0; i<3; i++) {
+        printf("Aluno %d\n", i+1);
+        printf("Nome: ");
+        fgets(aluno[i].nome, 100, stdin);
+        aluno[i].nome[strcspn(aluno[i].nome, "\n")] = '\0';
+        printf("Matricula: ");
+        fgets(aluno[i].matricula, 10, stdin);
+        aluno[i].matricula[strcspn(aluno[i].matricula, "\n")] = '\0';
+        printf("Nota 1: ");
+        scanf("%f", &aluno[i].nota1);
+        printf("Nota 2: ");
+        scanf("%f", &aluno[i].nota2);
+        printf("Nota 3: ");
+        scanf("%f", &aluno[i].nota3);
+        while (getchar() != '\n');
+    }
+    for (int i=0; i<3; i++) {
+        double media = (aluno[i].nota1 + aluno[i].nota2 + aluno[i].nota3) / 3.0;
+        if (media>=6) {
+            printf("Aluno %d APROVADO\n", i+1);
+        } else {
+            printf("Aluno %d REPROVADO\n", i+1);
+        }
+        if (media>=maiorMedia) {
+            maiorMediaAluno = i+1;
+            maiorMedia=media;
+        }
+        if (media<=menorMedia) {
+            menorMediaAluno = i+1;
+            menorMedia=media;
+        }
+        if (aluno[i].nota1>=maiorNota) {
+            maiorNotaAluno = i+1;
+            maiorNota=aluno[i].nota1;
+        }
+    }
+    printf("maior nota primeira prova: aluno %d\n", maiorNotaAluno);
+    printf("maior media: aluno %d\n", maiorMediaAluno);
+    printf("menor media: aluno %d\n", menorMediaAluno);
+
+    return 0;
+}
+-----------------------------------------------------------------------------------------------------------------------
+06)
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char matricula[10], nome[100];
+    long codigoDisciplina;
+    float nota1, nota2;
+    float media;
+}Alunos;
+
+int main () {
+    Alunos aluno[2];
+    printf("");
+
+    for (int i=0; i<2; i++) {
+        printf("Nome do aluno %d: ", i+1);
+        fgets(aluno[i].nome, 100, stdin);
+        aluno[i].nome[strcspn(aluno[i].nome, "\n")] = '\0';
+        printf("Matricula do aluno %d: ", i+1);
+        fgets(aluno[i].matricula, 100, stdin);
+        aluno[i].nome[strcspn(aluno[i].nome, "\n")] = '\0';
+        printf("Código da disciplina: ");
+        scanf("%li", &aluno[i].codigoDisciplina);
+        printf("Nota 1: ");
+        scanf("%f", &aluno[i].nota1);
+        printf("Nota 2: ");
+        scanf("%f", &aluno[i].nota2);
+        while (getchar() != '\n');
+    }
+
+    for (int i=0; i<2; i++) {
+        aluno[i].media = (aluno[i].nota1 + aluno[i].nota2) /2;
+        printf("Aluno %s\n", aluno[i].nome);
+        printf("Media: %.2f\n", aluno[i].media);
+    }
+
+    return 0;
+}
 */
